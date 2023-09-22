e91c6abab12f74299c1938a1b551339f6b89ad27
beezthebee <kinga.slota@op.pl>
2023-09-22 23:34:22 +0200

    Added customization

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 
 (trainSet, trainLabels), (testSet, testLabels) = tf.keras.datasets.mnist.load_data()
 
-# You can resize the training set 
+# --- RESIZE THE TRAINING SET HERE ---
 # trainSet = np.resize(trainSet, (1000, 28, 28))
 # trainLabels = np.resize(trainLabels, 1000)
 
-# You can customize the learning rate and number of epochs
+# --- CUSTOMIZE THE LEARNING RATE AND NUMBER OF EPOCHS HERE ---
 network = Network(trainSet, trainLabels, testSet, testLabels, 0.001, 10)
 network.trainNetwork()
